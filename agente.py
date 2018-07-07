1c5cbef140b3a07e4c3fa6c8aef69f301fb69032
jeiferp <32427182+jeiferp@users.noreply.github.com>
2018-07-07 17:26:33 -0600

    Agente.py y cambios en prueba

---
+++ agente.py
@@ -0,0 +1,12 @@
+class Agente:
+
+      def __init__(self,infection_rate = 4.3,initial_probability_of_death=0.548,initial_probability_of_chromatin_condensation=0.3,mNeptune_effectiveness=294.63,viral_reach=2.5,initial_infected_cell_percentage = 4.527,cell_density= 5.48):
+            self.infection_rate = infection_rate 
+            self.initial_probability_of_death =initial_probability_of_death
+            self.initial_probability_of_chromatin_condensation = initial_probability_of_chromatin_condensation
+            self.mNeptune_effectiveness = mNeptune_effectiveness
+            self.viral_reach = viral_reach
+            self.initial_infected_cell_percentage = initial_infected_cell_percentage
+            self.cell_density = cell_density      
+
+      
--- prueba.py
+++ prueba.py
@@ -2,6 +2,7 @@ import os
 import pyNetLogo
 from multiprocessing import Process
 from datetime import datetime
+from agente import Agente
 
 class Timer:
 	"""
@@ -22,30 +23,44 @@ class Timer:
 
 	def reset_timer(self):
 		self.start = datetime.now()
-
-timer = Timer()
-
-
 class netlogoComm:
     
-    def run(self,name):
-            timer.log("Start Thread")
-            print("set id " +str(name))
+    def run(self,index):
+            #timer.log("Start Thread")
+            print("set id " +str(index))
             netlogo = pyNetLogo.NetLogoLink(gui=False)
             netlogo.load_model(r'simulacion.nlogo')
             z = "set id " +str(1)
-            for x, y in zip(self.params, self.values):
-                netlogo.command(self.mode+" "+str(x)+" "+str(y))
+            netlogo.command(self.comandos[0]+str(self.poblacion[index].infection_rate))
+            netlogo.command(self.comandos[1]+str(self.poblacion[index].initial_probability_of_death))
+            netlogo.command(self.comandos[2]+str(self.poblacion[index].initial_probability_of_chromatin_condensation))
+            netlogo.command(self.comandos[3]+str(self.poblacion[index].mNeptune_effectiveness))
+            netlogo.command(self.comandos[4]+str(self.poblacion[index].viral_reach))
+            netlogo.command(self.comandos[5]+str(self.poblacion[index].initial_infected_cell_percentage))
+            netlogo.command(self.comandos[6]+str(self.poblacion[index].cell_density))
             netlogo.command('setup')
+            #timer.log("Final process")
             netlogo.repeat_command('go', 5)
             print("Finished Model")
-            timer.log("Final Thread")
+            #timer.log("Final Thread")
 
-    def __init__(self):  
+    def set_commands(self):
+        self.comandos.append("set infection-rate ")
+        self.comandos.append("set initial-probability-of-death ")
+        self.comandos.append("set initial-probability-of-chromatin-condensation ")
+        self.comandos.append("set mNeptune-effectiveness ")
+        self.comandos.append("set viral-reach ")
+        self.comandos.append("set initial-infected-cell-percentage ")
+        self.comandos.append("set cell-density ")
+
+    def __init__(self):
+            self.poblacion = []
+            self.comandos = []
+            self.set_commands()
             timer.log("Inicio")
             self.mode = "set"
-            self.params = ["infection-rate","initial-probability-of-death","initial-probability-of-chromatin-condensation","mNeptune-effectiveness","viral-reach","initial-infected-cell-percentage","cell-density"]
-            self.values = [4.3,0.548,0.3,294.63,2.5,4.527,5.48]
+            for contador in range(10):
+                self.poblacion.append(Agente())   
             print("Created Default Config")
             for contador in range(1):
                 timer.log("Setup")
